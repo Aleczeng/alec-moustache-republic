42d35514a583e570763234c582378a0eee52fb92
AlecZeng <zxh93321@gmail.com>
2019-07-31 10:10:06 +1200

    fix bug

--- src/basic/Notification.js
+++ src/basic/Notification.js
@@ -2,7 +2,6 @@ import Alert from "react-s-alert";
 
 export const handleAddProductSuccess = (e) => {
     e.preventDefault();
-    console.log('work');
     Alert.success('Add product to cart', {
         position: 'top-right',
         effect: 'slide',
@@ -12,7 +11,6 @@ export const handleAddProductSuccess = (e) => {
 
 export const handleAddProductError = (e) => {
     e.preventDefault();
-    console.log('work');
     Alert.error('Please select a size', {
         position: 'top-right',
         effect: 'slide',
